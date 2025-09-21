e93b6eb33229bcb38d9ee715966eb88d339824f6
ArtyomOl <artem.andreevich.olenev@mail.ru>
2025-09-21 18:35:38 +0300

    add basis for api

---
+++ api/service.py
@@ -0,0 +1,25 @@
+import typing
+
+from fastapi import FastAPI
+
+import schemas
+
+app = FastAPI(title='X5 entity allocator')
+
+@app.post('/api/predict')
+async def api_predict_post(request:schemas.PredictRequestModel = None):
+    if not request:
+        return ''
+    response_item = _create_response(request)
+    return [response_item]
+
+
+def _create_response(request:schemas.PredictRequestModel) -> schemas.PredictResponseItem:
+
+    pass
+
+    return schemas.PredictResponseItem(
+            start_index=0, 
+            end_index=0, 
+            entity=request.input
+        ).model_dump_json()
